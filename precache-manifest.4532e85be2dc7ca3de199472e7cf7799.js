b9a12bc5aba6d58823dba5b3b7ee22ddc5399b16
Ken Ting <kaleungting@gmail.com>
2021-07-06 22:18:23 -0400

    Updates

---
+++ precache-manifest.4532e85be2dc7ca3de199472e7cf7799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9a551e112389c4f8ec4",
+    "url": "/hooks-todo/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "b9a551e112389c4f8ec4",
+    "url": "/hooks-todo/static/js/main.b9a551e1.chunk.js"
+  },
+  {
+    "revision": "1dc9c6f50fbb1c56c5b6",
+    "url": "/hooks-todo/static/js/runtime~main.1dc9c6f5.js"
+  },
+  {
+    "revision": "658575c4d75d8922678d",
+    "url": "/hooks-todo/static/css/2.6390be7d.chunk.css"
+  },
+  {
+    "revision": "658575c4d75d8922678d",
+    "url": "/hooks-todo/static/js/2.658575c4.chunk.js"
+  },
+  {
+    "revision": "70c9042d73724a72c834c945661c6c5b",
+    "url": "/hooks-todo/index.html"
+  }
+];
